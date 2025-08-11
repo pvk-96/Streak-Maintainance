ece788c7fc3ec5814c36fe3c3190210faa1b4d54
PRANEETH VARMA KOPPERLA <praneethvarmakopperla@gmail.com>
2025-08-11 18:16:33 +0530

    Create Array.java

---
+++ Java/Array.java
@@ -0,0 +1,19 @@
+import java.util.*;
+// java Program to create an array and show its properties
+// 23331A0596 K. Praneeth Varma 04-07-2025.
+public class Arr{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter size of array");
+		int sz= sc.nextInt();
+		int num[] = new int[sz];
+		System.out.println("Enter the elements of array:");
+		for(int i=0; i<sz; i++){
+			num[i]= sc.nextInt();
+			}
+		System.out.print("The Elements of the array are: ");
+		for(int i=0; i<sz; i++){
+			System.out.println(num[i]);
+			}
+			}
+			}		
